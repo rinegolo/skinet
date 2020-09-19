6e73d7bfd7c1fea6f826b6b9c36a87bc78924836
rinegolo <rinegolo@gmail.com>
2020-09-19 10:39:05 -0400

    updating ng

--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -9,17 +9,11 @@ export class LoadingInterceptor implements HttpInterceptor {
     constructor(private busyService: BusyService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (req.method === 'POST' && req.url.includes('orders')) {
-            return next.handle(req);
+        if (!req.url.includes('emailexists')) {
+            this.busyService.busy();
         }
-        if (req.method === 'DELETE') {
-            return next.handle(req);
-        }
-        if (req.url.includes('emailexists')) {
-            return next.handle(req);
-        }
-        this.busyService.busy();
         return next.handle(req).pipe(
+            delay(1000),
             finalize(() => {
                 this.busyService.idle();
             })
